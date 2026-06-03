5e906305d964a4190c2cfd3447b49999551b1c61
Prashansa0410 <prashgupta0410@gmail.com>
2026-06-03 22:00:45 +0530

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.java
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        intervals.sort((a,b)->Integer.compare(a.start,b.start));
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for(Interval interval:intervals){
+            if(!queue.isEmpty() && queue.peek()<=interval.start){
+                queue.poll();
+            }
+            queue.offer(interval.end);
+        }
+        return queue.size();
+
+
+
+    }
+}
